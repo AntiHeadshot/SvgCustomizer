a1773fed66c3943c63cdb549a1dfdf1f71642967
Tobias Dahmen <alias.Slash@yahoo.de>
2021-11-22 23:54:06 +0100

    better darkmode (on by default)

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const RootComponent = {
     data() {
         return {
-            darkMode: false,
+            darkMode: true,
             svgUrl: "",
             svgFile: "",
             svg: {
@@ -12,35 +12,37 @@ const RootComponent = {
                 { name: "testParam1", type: "text", value: "Hello" },
                 { name: "testParam2", type: "number", value: 10 },
                 { name: "testParam3", type: { name: 'range', min: 30, max: 100 }, value: 80 }
-            ],
+            ]
         };
     },
     methods: {
         getSvg() {
             let values = [{ name: "none", value: "none" },
-            { name: "default", value: "" },
-            { name: "black", value: "black" },
-            { name: "red", value: "red" },
-            { name: "blue", value: "blue" },
-            { name: "#red", value: "#ff0000" },
-            { name: "#blue", value: "#0000ff" },
-            { name: "#r", value: "#f00" },
-            { name: "#b", value: "#00f" },
-            { name: "light", value: "#b5b5b5" },
-            { name: "dark", value: "#4b4b4b" },
-            { name: "green", value: "lawngreen" }];
+                { name: "default", value: "" },
+                { name: "black", value: "black" },
+                { name: "red", value: "red" },
+                { name: "blue", value: "blue" },
+                { name: "#red", value: "#ff0000" },
+                { name: "#blue", value: "#0000ff" },
+                { name: "#r", value: "#f00" },
+                { name: "#b", value: "#00f" },
+                { name: "light", value: "#b5b5b5" },
+                { name: "dark", value: "#4b4b4b" },
+                { name: "green", value: "lawngreen" },
+                { name: "purple", value: "purple" }
+            ];
 
             let width = 20 + 34 * values.length;
 
             let svg = `<svg viewBox="-20 -20 ${width} ${width}">
-<text font-size="6" text-anchor="end" transform="translate(-4 2) rotate(45)">stroke</text>
-<text font-size="6" text-anchor="end" transform="translate(2 -4) rotate(45)">fill</text>
+<text font-size="6" font-weight="bold" text-anchor="end" transform="translate(-4 2) rotate(30)">stroke</text>
+<text font-size="6" font-weight="bold" text-anchor="end" transform="translate(2 -4) rotate(30)">fill</text>
 `;
-            let pos = 8
+            let pos = 20;
             for (let val of values) {
                 svg += `<text font-size="6" text-anchor="end" transform="translate(-2 ${pos}) rotate(30)">${val.name}</text>
 `;
-                svg += `<text font-size="6" text-anchor="end" transform="translate(${pos} -2) rotate(60)">${val.name}</text>
+                svg += `<text font-size="6" text-anchor="end" transform="translate(${pos} -2) rotate(30)">${val.name}</text>
 `;
                 pos += 34;
             }
@@ -57,41 +59,47 @@ const RootComponent = {
                         svg += `<circle r="15" cx="${fPos}" cy="${sPos}" style="stroke:${stroke.value}"></circle>`;
                     else
                         svg += `<circle r="15" cx="${fPos}" cy="${sPos}" style=""></circle>`;
-                    sPos += 43;
+                    sPos += 34;
                 }
                 fPos += 34;
             }
-
             svg += "</svg>";
             return svg;
         },
         forceRerender() {
             this.componentKey += 1;
         },
+        loadFile() {
+            this.$refs.fileInput.click();
+        },
         updateFile(event) {
             if (event.target.files) {
+                this.svgFile = event.target.files[0].name;
+
+                console.log("load file");
                 var reader = new FileReader();
                 reader.onload = (evt) => {
                     this.svg = {
                         svg: evt.target.result,
-                        name: event.target.files[0].name
+                        name: this.svgFile
                     };
                     this.parameters = [];
                 };
                 reader.readAsText(event.target.files[0], "UTF-8");
+
+                event.target.value = null;
             }
-        }
+        },
     },
     watch: {
         darkMode(newValue, oldValue) {
-            console.log("mode changed");
             if (newValue)
                 document.getElementsByTagName('html')[0].classList.add('darkmode');
             else
                 document.getElementsByTagName('html')[0].classList.remove('darkmode');
-        }
-        , svg(newValue, oldValue) {
-            let parameterRegex = /\{\{(?<name>[^:}]*)(:(?<type>[^|}]*))?(\|(?<defaultValue>[^}]*))?\}\}/gm;
+        },
+        svg(newValue, oldValue) {
+            let parameterRegex = /\{\{(?<name>[^:}|]*)(:(?<type>[^|}]*))?(\|(?<defaultValue>[^}]*))?\}\}/gm;
             let match;
             let parameters = [];
 
@@ -101,28 +109,39 @@ const RootComponent = {
                 return "?";
             }
 
+            let functionRegex = /(?<=\()[^)]*(?=\))/;
+
             function getType(type, defaultValue) {
                 if (!type)
                     return 'text';
-                if (type.startsWith('range')) {
+
+                let functionMatch = functionRegex.exec(type);
+
+                if (functionMatch) {
                     let retType = {
-                        type: 'range',
-                        min: 0,
-                        max: 0
+                        name: type.split('(')[0]
                     };
 
-                    let rangeRegex = /range\( *(?<min>\d+) *(, *(?<max>\d+) *)?\))/;
-                    let rangeMatch = rangeRegex.exec(type);
+                    let functionParameterRegex = /(?<name>[^,:]*):(?<value>[^,]*)/g;
 
-                    if (!rangeMatch.groups.min) {
-                        retType.min = 0;
-                        retType.max = Math.pow(10, Math.ceil(Math.log10(Number(defaultValue))));
-                    } else if (!rangeMatch.groups.max) {
-                        retType.min = 0;
-                        retType.max = rangeMatch.groups.min;
-                    } else {
-                        retType.min = rangeMatch.groups.min;
-                        retType.max = rangeMatch.groups.max;
+                    let params;
+
+                    while (params = functionParameterRegex.exec(functionMatch[0])) {
+                        console.log(params);
+                        retType[params.groups.name] = params.groups.value;
+                    }
+                    console.log(retType);
+
+                    if (retType.type == 'range') {
+                        if (!retType.min && !retType.max) {
+                            retType.min = 0;
+                            retType.max = Math.pow(10, Math.ceil(Math.log10(Number(defaultValue))));
+                        } else if (!retType.min && retType.max) {
+                            retType.max = Math.pow(10, Math.ceil(Math.log10(Math.max(Number(defaultValue), retType.min))));
+                        } else if (!retType.max) {
+                            retType.max = retType.min;
+                            retType.min = 0;
+                        }
                     }
 
                     return retType;
@@ -156,7 +175,7 @@ app.component("parameter", {
     <div> {{name}}
         <input v-if="type === 'text'" v-model="value" />
         <input v-else-if="type === 'number'" v-model="value" type="number" />
-        <div v-else-if="type.name === 'range'" style="display:inline-block"><input v-model="value" type="range" :min="type.min" :max="type.max" /> {{value}}</div>
+        <div v-else-if="type.name === 'range'" style="display:inline-block"><input v-model="value" type="range" :min="type.min" :max="type.max" /> <input v-model="value" type="number" :min="type.min" :max="type.max" /></div>
         <div v-else> unknown Type ({{(typeof type)}})'{{type}}'</div>
     </div>
     `,
@@ -178,7 +197,7 @@ app.component("parameter", {
 });
 
 app.component("svg-renderer", {
-    template: `<div><button @click="saveSvg()">save</button><div v-html="renderedSvg"></div><div style="opacity: 0" ref="svgMeasure"></div></div>`,
+    template: `<div><button @click="saveSvg()" title="save"><i class="fas fa-file-export"></i></button><div v-html="renderedSvg"></div><div style="opacity: 0" ref="svgMeasure"></div></div>`,
     props: {
         parameters: Array,
         svg: Object,
@@ -193,7 +212,7 @@ app.component("svg-renderer", {
             let renderedSvg = this.svg.svg;
             for (let param of this.parameters) {
                 do {
-                    var match = new RegExp(`\{\{(${param.name})(:[^}]*)?\}\}`, 'gm').exec(renderedSvg);
+                    var match = new RegExp(`\{\{(${param.name})(:[^}]*)?(|[^}]*)?\}\}`, 'gm').exec(renderedSvg);
                     if (match) {
                         renderedSvg = renderedSvg.substr(0, match.index) + param.value + renderedSvg.substr(match.index + match[0].length);
                     }
@@ -203,7 +222,10 @@ app.component("svg-renderer", {
 
             if (this.$refs.svgMeasure) {
                 this.$refs.svgMeasure.innerHTML = renderedSvg;
-                let bbox = this.$refs.svgMeasure.children[0].getBBox();
+                let bbox = this.$refs.svgMeasure.children[0].getBBox({ stroke: true });
+                bbox.width = Math.max(0.1, bbox.width);
+                bbox.height = Math.max(0.1, bbox.height);
+
                 let viewBox = `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`;
 
                 var parser = new DOMParser();
