a320d599019c3f9aeca2b1e6d8272532d2892ad3
Tobias Dahmen <github@dahmen.space>
2021-11-23 19:14:52 +0100

    added calculations with {{{}}}
    possible code excecution... be carefull.

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,47 @@
 const RootComponent = {
     data() {
+        let svg = this.getSvg();
         return {
             darkMode: true,
+            allowCode: false,
             svgUrl: "",
             svgFile: "",
             svg: {
-                svg: this.getSvg(),
+                svg,
                 name: 'defaultFile.svg'
             },
-            parameters: [
-                { name: "testParam1", type: "text", value: "Hello" },
-                { name: "testParam2", type: "number", value: 10 },
-                { name: "testParam3", type: { name: 'range', min: 30, max: 100 }, value: 80 }
-            ]
+            parameters: this.getParameters(svg)
         };
     },
     methods: {
         getSvg() {
+
+            return `<svg>
+            <line x1="0" x2="300" y1="0" y2="0"/>
+            <text x="10" y="10">{{m:range(min:0.1,max:2,step:0.1)|1}}</text>
+            <text x="10" y="30">{{b:range(min:1,max:10)|5}}</text>
+            <text x="10" y="50">{{c:range(min:0,max:10,step:0.1)|0}}</text>
+            <text x="10" y="70">m+b = {{{m+b}}}</text>
+            <text x="10" y="90">m*b = {{{m*b}}}</text>
+            <text x="10" y="110">plot = m*(x+c)²+b:</text>
+            <g transform="translate(10 210)">
+                <path d="{{{ (()=>{var d=''; console.log(c); for(var x=0;x<=100;x++){d+=(x==0?'M':'L')+x+' '+(100-((x+c)*(x+c)*m+b*100)/100);} return d;})() }}}" stroke="red" fill="none" stroke-width="1"/>
+            </g>
+            </svg>`;
+
             let values = [{ name: "none", value: "none" },
-                { name: "default", value: "" },
-                { name: "black", value: "black" },
-                { name: "red", value: "red" },
-                { name: "blue", value: "blue" },
-                { name: "#red", value: "#ff0000" },
-                { name: "#blue", value: "#0000ff" },
-                { name: "#r", value: "#f00" },
-                { name: "#b", value: "#00f" },
-                { name: "light", value: "#b5b5b5" },
-                { name: "dark", value: "#4b4b4b" },
-                { name: "green", value: "lawngreen" },
-                { name: "purple", value: "purple" }
+            { name: "default", value: "" },
+            { name: "black", value: "black" },
+            { name: "red", value: "red" },
+            { name: "blue", value: "blue" },
+            { name: "#red", value: "#ff0000" },
+            { name: "#blue", value: "#0000ff" },
+            { name: "#r", value: "#f00" },
+            { name: "#b", value: "#00f" },
+            { name: "light", value: "#b5b5b5" },
+            { name: "dark", value: "#4b4b4b" },
+            { name: "green", value: "lawngreen" },
+            { name: "purple", value: "purple" }
             ];
 
             let width = 20 + 34 * values.length;
@@ -76,8 +88,7 @@ const RootComponent = {
             if (event.target.files) {
                 this.svgFile = event.target.files[0].name;
 
-                console.log("load file");
-                var reader = new FileReader();
+                let reader = new FileReader();
                 reader.onload = (evt) => {
                     this.svg = {
                         svg: evt.target.result,
@@ -90,22 +101,20 @@ const RootComponent = {
                 event.target.value = null;
             }
         },
-    },
-    watch: {
-        darkMode(newValue, oldValue) {
-            if (newValue)
-                document.getElementsByTagName('html')[0].classList.add('darkmode');
-            else
-                document.getElementsByTagName('html')[0].classList.remove('darkmode');
-        },
-        svg(newValue, oldValue) {
-            let parameterRegex = /\{\{(?<name>[^:}|]*)(:(?<type>[^|}]*))?(\|(?<defaultValue>[^}]*))?\}\}/gm;
+        getParameters(svg) {
+            let parameterRegex = /\{\{(?<name>[^:}|]*)(:(?<type>[^|}]*))?(\|(?<defaultValue>[^}]*))?\}\}(?=[^}])/gm;
             let match;
             let parameters = [];
 
             function getValue(type, defaultValue) {
-                if (defaultValue)
+                if (defaultValue) {
+                    if (type && type.name)
+                        switch (type.name) {
+                            case 'range':
+                                return Number(defaultValue);
+                        }
                     return defaultValue;
+                }
                 return "?";
             }
 
@@ -127,10 +136,8 @@ const RootComponent = {
                     let params;
 
                     while (params = functionParameterRegex.exec(functionMatch[0])) {
-                        console.log(params);
                         retType[params.groups.name] = params.groups.value;
                     }
-                    console.log(retType);
 
                     if (retType.type == 'range') {
                         if (!retType.min && !retType.max) {
@@ -142,6 +149,8 @@ const RootComponent = {
                             retType.max = retType.min;
                             retType.min = 0;
                         }
+                        if (!retType.step)
+                            retType.step = 1;
                     }
 
                     return retType;
@@ -150,20 +159,32 @@ const RootComponent = {
             }
 
             do {
-                match = parameterRegex.exec(this.svg.svg);
+                match = parameterRegex.exec(svg);
                 if (match) {
-                    let param = {
-                        name: match.groups.name,
-                        type: getType(match.groups.type, match.groups.defaultValue),
-                        value: getValue(match.groups.type, match.groups.defaultValue)
-                    };
+                    let param = {};
+                    param.name = match.groups.name;
+                    param.type = getType(match.groups.type, match.groups.defaultValue);
+                    param.value = getValue(param.type, match.groups.defaultValue);
 
                     if (!parameters.find(x => x.name == param.name))
                         parameters.push(param);
                 }
             } while (match);
 
-            this.parameters = parameters;
+            console.log(parameters);
+            return parameters;
+        }
+
+    },
+    watch: {
+        darkMode(newValue, oldValue) {
+            if (newValue)
+                document.getElementsByTagName('html')[0].classList.add('darkmode');
+            else
+                document.getElementsByTagName('html')[0].classList.remove('darkmode');
+        },
+        svg(newValue, oldValue) {
+            this.parameters = getParameters(this.svg.svg);
         }
     }
 }
@@ -174,8 +195,11 @@ app.component("parameter", {
     template: `
     <div> {{name}}
         <input v-if="type === 'text'" v-model="value" />
-        <input v-else-if="type === 'number'" v-model="value" type="number" />
-        <div v-else-if="type.name === 'range'" style="display:inline-block"><input v-model="value" type="range" :min="type.min" :max="type.max" /> <input v-model="value" type="number" :min="type.min" :max="type.max" /></div>
+        <input v-else-if="type === 'number'" v-model.number="value" type="number" />
+        <div v-else-if="type.name === 'range'" style="display:inline-block">
+            <input v-model.number="value" type="range" :min="type.min" :max="type.max" :step="type.step" />
+            <input v-model.number="value" type="number" :min="type.min" :max="type.max"  :step="type.step" />
+        </div>
         <div v-else> unknown Type ({{(typeof type)}})'{{type}}'</div>
     </div>
     `,
@@ -196,11 +220,24 @@ app.component("parameter", {
     }
 });
 
+function evalInScope(js, contextAsScope) {
+    //# Return the results of the in-line anonymous function we .call with the passed context
+    return function () { with (this) { return eval(js); }; }.call(contextAsScope);
+}
+
 app.component("svg-renderer", {
-    template: `<div><button @click="saveSvg()" title="save"><i class="fas fa-file-export"></i></button><div v-html="renderedSvg"></div><div style="opacity: 0" ref="svgMeasure"></div></div>`,
+    template: `
+<div>
+    <button @click="saveSvg()" title="save" style="margin-bottom:20px">
+        <i class="fas fa-file-export"></i>
+    </button>
+    <div v-html="renderedSvg"></div>
+    <div style="opacity: 0" ref="svgMeasure"></div>
+</div>`,
     props: {
         parameters: Array,
         svg: Object,
+        allowCode: Boolean
     },
     data() {
         return {
@@ -210,9 +247,37 @@ app.component("svg-renderer", {
     methods: {
         renderSvg() {
             let renderedSvg = this.svg.svg;
-            for (let param of this.parameters) {
+            //replace calculations
+            if (this.allowCode) {
+                let scope = {};
+                for (let param of this.parameters)
+                    scope[param.name] = param.value;
+
+                let match;
                 do {
-                    var match = new RegExp(`\{\{(${param.name})(:[^}]*)?(|[^}]*)?\}\}`, 'gm').exec(renderedSvg);
+                    match = new RegExp(`\{\{\{(?<calc>.*?)\}\}\}`, 'gm').exec(renderedSvg);
+                    if (match) {
+                        let result = evalInScope(match.groups.calc, scope);
+                        renderedSvg = renderedSvg.substr(0, match.index) + result + renderedSvg.substr(match.index + match[0].length);
+                    }
+                }
+                while (match);
+            } else {
+                let match
+                do {
+                    match = new RegExp(`\{\{\{([^}]*)\}\}\}`, 'gm').exec(renderedSvg);
+                    if (match) {
+                        renderedSvg = renderedSvg.substr(0, match.index) + "" + renderedSvg.substr(match.index + match[0].length);
+                    }
+                }
+                while (match);
+            }
+
+            for (let param of this.parameters) {
+                //replace parameters
+                let match
+                do {
+                    match = new RegExp(`\{\{(${param.name})(:[^}]*)?(|[^}]*)?\}\}`, 'gm').exec(renderedSvg);
                     if (match) {
                         renderedSvg = renderedSvg.substr(0, match.index) + param.value + renderedSvg.substr(match.index + match[0].length);
                     }
@@ -228,12 +293,12 @@ app.component("svg-renderer", {
 
                 let viewBox = `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`;
 
-                var parser = new DOMParser();
-                var xmlDoc = parser.parseFromString(renderedSvg, "text/xml");
+                let parser = new DOMParser();
+                let xmlDoc = parser.parseFromString(renderedSvg, "text/xml");
 
                 xmlDoc.children[0].setAttribute("viewBox", viewBox);
 
-                var serializer = new XMLSerializer();
+                let serializer = new XMLSerializer();
                 renderedSvg = serializer.serializeToString(xmlDoc);
             }
 
@@ -243,13 +308,13 @@ app.component("svg-renderer", {
             this.triggerDownload('data:image/svg+xml;base64,' + Base64.encode(this.renderedSvg));
         },
         triggerDownload(imgURI) {
-            var evt = new MouseEvent('click', {
+            let evt = new MouseEvent('click', {
                 view: window,
                 bubbles: false,
                 cancelable: true
             });
 
-            var a = document.createElement('a');
+            let a = document.createElement('a');
             a.setAttribute('download', this.svg.name);
             a.setAttribute('href', imgURI);
             a.setAttribute('target', '_blank');
@@ -264,7 +329,7 @@ app.component("svg-renderer", {
             },
             deep: true,
             immediate: true,
-        },
+        }
     }
 });
 
